578ceffd96c4c92008b45e8535fbc4368338b953
Nickolas Laforest <laforest.nickolas@gmail.com>
2020-11-20 11:12:19 -0500

    Premier commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,53 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+//  code qui permet de faire que le jeu créé un conteneur pour le jeu.
 
+class Jeu {
+
+    constructor() {
+       console.log("création du jeu") ;
+
+    }
+
+
+
+
+}
+
+// code qui permet de faire que le jeu créé un objet qui fera office de serpent et augmente sa longueur si il avale
+// une pomme.
+
+
+    class Serpent {
+
+        constructor() {
+            console.log("création du serpent");
+
+        }
+
+
+
+
+    }
+
+
+//code qui permet de faire que le jeu créé un objet pomme qui change de place à chaque fois que le serpent avale
+// la précédente.
+
+
+    class Pomme {
+
+        constructor() {
+            console.log("création de la pomme");
+
+        }
+
+
+
+
+    }
+
+
+    var unePartie = new Jeu();
 });
